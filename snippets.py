af67bb231021bab1dd6b5e54b5c04f12f5ca9821
jugoretz <jugoretz@gmail.com>
2013-10-13 16:15:45 -0400

    Revert "no hip"
    This reverts commit b0f7cecd028c49e2c7741473f4bfb86967826ab4.

--- snippets.py
+++ snippets.py
@@ -28,7 +28,7 @@ __author__ = 'Craig Silverstein <csilvers@khanacademy.org> modified for Macaulay
 
 
 # Have the cron jobs send to HipChat in addition to email?
-_SEND_TO_HIPCHAT = False
+_SEND_TO_HIPCHAT = True
 
 if _SEND_TO_HIPCHAT:
     import hipchatlib
